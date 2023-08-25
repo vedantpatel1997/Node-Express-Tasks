d7efeff6d3ecd0f72067607e7bdb5ceadb5dc38f
vedantpatel1997 <vedantp9@gmail.com>
2023-08-24 23:48:51 -0400

    Fix the Date issue and Improvise the code

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const { check, validationResult } = require("express-validator");
-var mongoose = require("mongoose");
 
 // Set up MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/finalExamVersion2");
 
-// Creating the model in the database
-const taskModal = mongoose.model("tasks", {
-  name: { type: String },
-  dynamicElement: { type: String },
-  dueDate: { type: String },
-  isComplete: { type: Boolean },
+// Define the task schema
+const taskSchema = new mongoose.Schema({
+  name: String,
+  dynamicElement: String,
+  dueDate: String,
+  isComplete: { type: Boolean, default: false },
 });
+
+// Create the task model
+const Task = mongoose.model("tasks", taskSchema);
 
 const app = express();
 
-// Static files middleware to serve CSS, JS, and images from the public folder
+// Serve static files from the public folder
 app.use(express.static(path.join(__dirname, "/public")));
 
 // Parse URL-encoded bodies (as sent by HTML forms)
@@ -31,140 +34,128 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-// Just Checking git Brnach
-
 // Form page route
-app.get("/Form", function (req, res) {
+app.get("/Form", (req, res) => {
   res.render("form");
 });
 
-app.post("/Form", function (req, res) {
-  const taskDescription = req.body.description;
-  const isDynamicallyDesigned = /^<h[1-5]>.*<\/h[1-5]>$/.test(taskDescription);
-  const containsNoItalics = !/<i>|<\/i>/.test(taskDescription);
+// Function to get today's date in "YYYY-MM-DD" format
+function todayDate() {
+  const dateToday = new Date();
+  const year = dateToday.getFullYear();
+  const month = String(dateToday.getMonth() + 1).padStart(2, "0");
+  const day = String(dateToday.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
 
-  if (isDynamicallyDesigned && containsNoItalics) {
-    const date = new Date(req.body.date);
-    let day = date.getDate();
-    let month = date.getMonth();
-    let year = date.getFullYear();
+// Handle form submission
+app.post("/Form", (req, res) => {
+  const task = new Task({
+    name: req.body.name,
+    dynamicElement: req.body.description,
+    dueDate: req.body.date,
+  });
 
-    // This arrangement can be altered based on how we want the date's format to appear.
-    let currentDate = `${day + 1}-${month + 1}-${year}`;
-
-    let task = new taskModal({
-      name: req.body.name,
-      dynamicElement: taskDescription,
-      dueDate: currentDate,
+  task
+    .save()
+    .then(() => {
+      res.render("form");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred.");
     });
-
-    task
-      .save()
-      .then(() => {
-        res.render("form");
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("An error occurred.");
-      });
-  } else {
-    res.render("form", {
-      message:
-        "There were some errors in the form while making the Task. It should have H1 tag and Not any Italic Character.",
-    });
-  }
 });
 
 // Tasks page route
-app.get("/Tasks", function (req, res) {
-  taskModal.find({}).then((tasks) => {
-    console.log("tasks: ", tasks);
+app.get("/Tasks", (req, res) => {
+  Task.find({}).then((tasks) => {
     res.render("tasks", { tasks });
   });
 });
 
-// Tasks page route
-app.get("/TodayTask", function (req, res) {
-  const date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth();
-  let year = date.getFullYear();
-
-  let currentDate = `${day}-${month + 1}-${year}`;
-  console.log('"currentDate:', currentDate);
-  taskModal
-    .find({
-      dueDate: currentDate,
-    })
-    .then((tasks) => {
-      res.render("tasks", { tasks });
-    });
-});
-
-// Tasks Delete route
-app.get("/delete/:id", function (req, res) {
-  taskModal
-    .deleteOne({ _id: req.params.id }) // Delete the question.
-    .then(res.redirect("/Tasks"));
-});
-
-// Tasks Edit Get route
-app.get("/viewOrEdit/:id", function (req, res) {
-  taskModal.findById({ _id: req.params.id }).then((task) => {
-    if (!task) {
-      res.status(404).send("Task not found");
-    } else {
-      res.render("form", { task });
-    }
+// TodayTask page route
+app.get("/TodayTask", (req, res) => {
+  Task.find({
+    dueDate: todayDate(),
+  }).then((tasks) => {
+    res.render("tasks", { tasks });
   });
 });
 
-// Tasks Edit Post route
-app.post("/viewOrEdit/:id", function (req, res) {
-  taskModal
-    .findOneAndUpdate(
-      { _id: req.params.id },
-      {
-        name: req.body.name,
-        dynamicElement: req.body.description,
-        dueDate: req.body.date,
-      },
-      { new: true }
-    )
+// Delete task route
+app.get("/delete/:id", (req, res) => {
+  Task.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.redirect("/Tasks");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred.");
+    });
+});
+
+// View/Edit task route (GET)
+app.get("/viewOrEdit/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then((task) => {
+      if (!task) {
+        res.status(404).send("Task not found");
+      } else {
+        res.render("form", { task });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred.");
+    });
+});
+
+// View/Edit task route (POST)
+app.post("/viewOrEdit/:id", (req, res) => {
+  Task.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      dynamicElement: req.body.description,
+      dueDate: req.body.date,
+    },
+    { new: true }
+  )
     .then((updatedTask) => {
-      res.redirect("/Tasks"); // Redirect to the tasks page after update
+      res.redirect("/Tasks");
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).send("Error updating task");
+      res.status(500).send("An error occurred.");
     });
 });
 
-// Tasks Complete route
-app.get("/Complete/:id", function (req, res) {
-  taskModal
-    .updateOne({ _id: req.params.id }, { isComplete: true })
+// Mark task as complete route
+app.get("/Complete/:id", (req, res) => {
+  Task.findByIdAndUpdate(req.params.id, { isComplete: true })
     .then(() => {
-      res.redirect("/Tasks"); // Redirect to the tasks page after update
+      res.redirect("/Tasks");
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).send("Error marking task as complete");
+      res.status(500).send("An error occurred.");
     });
 });
 
-// Tasks Not Complete route
-app.get("/NotComplete/:id", function (req, res) {
-  taskModal
-    .updateOne({ _id: req.params.id }, { isComplete: false })
+// Mark task as not complete route
+app.get("/NotComplete/:id", (req, res) => {
+  Task.findByIdAndUpdate(req.params.id, { isComplete: false })
     .then(() => {
-      res.redirect("/Tasks"); // Redirect to the tasks page after update
+      res.redirect("/Tasks");
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).send("Error marking task as complete");
+      res.status(500).send("An error occurred.");
     });
 });
 
-// Start the server on port 4235
-app.listen(4220, () => console.log("Server started on http://localhost:4220/"));
+// Start the server on port 4220
+app.listen(4220, () => {
+  console.log("Server started on http://localhost:4220/");
+});
